1afbacd553a35e5b64eaff911c8ca8593aeb6fe0
Jackiso2006 <isocomo06@gmail.com>
2024-01-13 10:36:20 +0100

    video per domenica 14

--- JS/script.js
+++ JS/script.js
@@ -1,53 +1,37 @@
-function init(){
-    $(document).ready(function () {
-        $("#newOpulento").hide();
-     });
+window.onload = () => {
+   $("#newOpulento").hide();
 
-     var pag = document.getElementById('pagina');
-     function cambiaSfondo() {
-        var body = document.getElementsById("pagina");
-        body.style.backgroundColor = "white";
-     }
+   const daysValue = document.querySelector("#days");
+   const hoursValue = document.querySelector("#hours");
+   const minutesValue = document.querySelector("#minutes");
+   const secondsValue = document.querySelector("#seconds");
 
-     var countDownDate = new Date("Jan 14, 2024 22:00:00").getTime();
-     const daysValue = document.querySelector("#days");
-     const hoursValue = document.querySelector("#hours");
-     const minutesValue = document.querySelector("#minutes");
-     const secondsValue = document.querySelector("#seconds");
+   function updateCountdown() {
+      // Find the distance between now and the count down date
+      let distance = new Date("Jan 14, 2024 22:00:00").getTime() - new Date().getTime();
 
-     // Update the count down every 1 second
-     var x = setInterval(function () {
+      // Time calculations for days, hours, minutes, and seconds
+      daysValue.innerText = Math.floor(distance / (1000 * 60 * 60 * 24));
+      hoursValue.innerText = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      minutesValue.innerText = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      secondsValue.innerText = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Get today's date and time
-        var now = new Date().getTime();
+      if (distance < 0) {
+         clearInterval(x); // ferma loop
+         $("#newOpulento").show();
+         $(".countdown-timer-container").hide();
+         document.querySelector("#video").play();
+      }
+   }
 
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        daysValue.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24));
-        hoursValue.innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        minutesValue.innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        secondsValue.innerHTML = Math.floor((distance % (1000 * 60)) / 1000);
-
-        if (distance < 0) {
-           clearInterval(x);
-           window.onload = cambiaSfondo;
-           $("#newOpulento").show();
-           $(".countdown-timer-container").hide();
-        }
-     }, 1000);
+   const x = setInterval(updateCountdown, 1000);
 }
 
-
-
-function initOrologi(){
-    window.onscroll = function () { scrollbar() };
-
-    function scrollbar() {
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
-        document.getElementById("myBar").style.width = scrolled + "%";
-    }
+function initOrologi() {
+   window.onscroll = ()=>{
+      var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+      var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      var scrolled = (winScroll / height) * 100;
+      document.getElementById("myBar").style.width = scrolled + "%";
+   };
 }
